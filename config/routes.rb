5edcb4eb31a71029da18152a48c83dcbaddb2b5a
ks1venberg <venberg.dev@gmail.com>
2019-03-29 20:49:40 +0300

    cleaned up routs file

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-root to: "home#index"
+	devise_for :users
+	
+	root to: "home#index"
 
-  # get 'home/index'
-  # get 'home/terms'
-  # get 'home/about'
-
-get 'terms' => 'pages#terms'
-get 'about' => 'pages#about'
-# get 'contacts' => 'contacts#new'
-# resource :contacts, only: [:create] 
+	get 'terms' => 'pages#terms'
+	get 'about' => 'pages#about'
 
 	resources :articles do
-    resources :comments
-  end
-  
-  resource :contacts, only: [:new, :create], path_names: {:new =>''}
+	resources :comments
+	end
+
+	resource :contacts, only: [:new, :create], path_names: {:new =>''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
